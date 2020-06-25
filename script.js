f807fca699358ad3d921068436cc38ee4767f052
Mo Pishdar <mohpish@gmail.com>
2020-06-25 21:41:23 +1000

    some minor changes

---
+++ script.js
@@ -0,0 +1,15 @@
+const firstSeacrhbox = $("#first-box");
+
+$.ajax({
+    url: "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=f960f7d3&app_key=8815afa06c70515964d774a471c8c248&results_per_page=20&what=javascript%20developer&content-type=application/json",
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
+
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=javascript&type=video&key=AIzaSyBJ0EE3BhTz86K0yopCqgn45JS7EBuybwA",
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+})
