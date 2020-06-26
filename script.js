359dffa3f137e64b0e810b3410ba241067ef06eb
Mo Pishdar <mohpish@gmail.com>
2020-06-27 00:31:38 +1000

    added all the possible parameters that could be included in a basic and complex search for later reference

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 // ============================================================================================================
 // Mo's Code
 // =========================================================================================================
+
+//possible parameters for a basic search
+//1. Number of retrieved results
+//2. keyword
+//possible parameters for a complex search
+//1. Minimum Salary
+//2. Searching for only full time jobs
+//3. Searching for only permanent positions
+//4. Excluding keywords that maycontain similar words like excluding ads for Java developers when you search for a Javascript job
+//5. Choosing the city (This version of Adzuna API only works for the UK so the city must be anywere in the UK)
+//6. Sorting the results by slaray 
+
 $.ajax({
     url: "https://api.adzuna.com/v1/api/jobs/gb/search/1?app_id=f960f7d3&app_key=8815afa06c70515964d774a471c8c248&results_per_page=20&what=javascript%20developer&content-type=application/json",
     method: "GET"
